845e21e28a36ee70f247efa9de3ac29fb7302780
Daisy Smith <daisymarie128@gmail.com>
2018-02-19 21:01:02 +1100

    basics of a drawing app

---
+++ src/ofApp.cpp
@@ -0,0 +1,78 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(0, 0, 0);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    for (auto &vert : line.getVertices()){
+        vert.x += ofRandom(-1 ,1);
+        vert.y += ofRandom(-1, 1);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    line.draw();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    ofPoint pt;
+    pt.set(x,y);
+    line.addVertex(pt);
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+//    ofPoint pt;
+//    pt.set(x,y);
+//    line.begin();
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+//    line.end();
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
